6431ff0aadc38d71533244ea1c0b591958314743
Ridwan <ridwanediallo@gmail.com>
2024-11-10 00:05:30 +0000

    Add create user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_09_193241) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_09_224920) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_09_193241) do
     t.string "name"
     t.text "explanation"
     t.text "examples"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
