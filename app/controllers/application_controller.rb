f40f5645175b7c1136eac616d5829c49dc02f94a
Ridwan <ridwanediallo@gmail.com>
2024-11-10 00:02:55 +0000

    Add authorize_request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  def authorize_request
+    token = request.headers["Authorization"]&.split(" ")&.last
+    begin
+      decoded = JWT.decode(token, Rails.application.secrets.secret_key_base, true, { algorithm: "HS256" })
+      @current_user = User.find(decoded[0]["user_id"])
+    rescue JWT::DecodeError
+      render json: { error: "Unauthorized access" }, status: :unauthorized
+    end
+  end
 end
