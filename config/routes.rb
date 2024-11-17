e9604827cdf98c03c7dad22a6298b68244afab41
Ridwan <ridwanediallo@gmail.com>
2024-11-17 15:14:58 +0000

    Add users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post "/register", to: "auth#register"
   post "/login", to: "auth#login"
+  resources :users, only: [ :index, :show, :create, :update, :destroy ]
   resources :phrases
 end
