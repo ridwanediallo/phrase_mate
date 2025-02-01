e27dbbd2a81c1133138a436cc3465f129dcf40d6
Ridwan <ridwanediallo@gmail.com>
2025-02-01 11:53:44 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   post "/register", to: "auth#register"
   post "/login", to: "auth#login"
   resources :users, only: [ :index, :show, :create, :update, :destroy ]
-  resources :phrases
-
+  resources :phrases do
+    collection do
+      get :search
+    end
+  end
+  resources :quizzes, only: [ :index, :create ] do
+    post :submit, on: :member
+  end
+  resources :tags, only: %i[index create update destroy]
   namespace :admin do
     resources :users, only: [ :create ]
   end
